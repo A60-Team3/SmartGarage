3c05380789fb04943ae5f0b01e29be1ddb1cb2a5
Milan Statev <milan_statev@yahoo.co.uk>
2024-08-21 16:01:28 +0300

    Initial commit

---
+++ src/test/java/org/example/smartgarage/SmartGarageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.smartgarage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartGarageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
